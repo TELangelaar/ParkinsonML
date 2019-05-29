a04aed8a1a8fce1b7ac2762d811b23022331aa6d
TELangelaar <t.e.langelaar@student.rug.nl>
2019-05-29 09:44:52 +0200

    Nu met het nieuwe script

--- ProjectParkinson.py
+++ ProjectParkinson.py
@@ -5,34 +5,40 @@ Created on Sat May  4 14:01:58 2019
 Time:
     04-05: 1358-1734
 """
+import os
 import pandas as pd
 import numpy as np
-
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 from readprocess_tappy import process_user
 
-import os
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, RobustScaler
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+
 
 import warnings; warnings.filterwarnings('ignore')
 
-#%% Directories
-directory_tappy = "D:/HMS/Master1/BigData/GitRepos/ParkinsonML/Tappy Data/"
-directory_user = "D:/HMS/Master1/BigData/GitRepos/ParkinsonML/Archived users/"
+# %% Directories
+directory_tappy = "Tappy Data/"
+directory_user = "Archived users/"
 
-#%% Data Import
+# %% Data Import
 user_data = os.listdir(directory_user)
 tappy_data = os.listdir(directory_tappy)
 
-ID_data = [x[5:15] for x in user_data] #Get user ID from file name
-ID_tappy = [x[0:10] for x in tappy_data]   
+ID_data = [x[5:15] for x in user_data]  # Get user ID from file name
+ID_tappy = [x[0:10] for x in tappy_data] 
 
-users_actual = [value for value in ID_data if value in ID_tappy] #If the user has no Tappy data, we dont need that user
+users_actual = [value for value in ID_data if value in ID_tappy]  # If the user has no Tappy data, we dont need that user
 
-#Import User Data
+# Import User Data
 columns = ['BirthYear', 'Gender', 'Parkinsons', 'Tremors', 'DiagnosisYear',
-    'Sided', 'UPDRS', 'Impact', 'Levadopa', 'DA', 'MAOB', 'Other']
+           'Sided', 'UPDRS', 'Impact', 'Levadopa', 'DA', 'MAOB', 'Other']
 user_df = pd.DataFrame(columns=columns)
 for userid in users_actual:
     f = open(directory_user+'User_'+userid+'.txt')
@@ -41,10 +47,12 @@ for userid in users_actual:
 
 user_df['BirthYear'] = pd.to_numeric(user_df['BirthYear'], errors='coerce')
 user_df['DiagnosisYear'] = pd.to_numeric(user_df['DiagnosisYear'], errors='coerce')
-user_df = user_df.rename(index=str, columns={'Gender': 'Male'}) # renaming `Gender` to `Female`
-user_df['Male'] = user_df['Male'] == 'Male' # change string data to boolean data
-user_df['Male'] = user_df['Male'].astype(int) # change boolean data to binary data
-str_to_bin_columns = ['Parkinsons', 'Tremors', 'Levadopa', 'DA', 'MAOB', 'Other'] # columns to be converted to binary data
+user_df = user_df.rename(index=str, columns={'Gender': 'Male'})  # renaming `Gender` to `Female`
+user_df['Male'] = user_df['Male'] == 'Male'  # change string data to boolean data
+user_df['Male'] = user_df['Male'].astype(int)  # change boolean data to binary data
+str_to_bin_columns = ['Parkinsons', 'Tremors',
+                      'Levadopa', 'DA', 'MAOB',
+                      'Other'] # columns to be converted to binary data
 
 for column in str_to_bin_columns:
     user_df[column] = user_df[column] == 'True'
@@ -55,16 +63,16 @@ user_df.loc[
     (user_df['Impact'] != 'Mild') &
     (user_df['Impact'] != 'Severe'), 'Impact'] = 'None'
 
-to_dummy_column_indices = ['Sided', 'UPDRS'] # columns to be one-hot encoded
+to_dummy_column_indices = ['Sided', 'UPDRS']  # columns to be one-hot encoded
 for column in to_dummy_column_indices:
     user_df = pd.concat([
         user_df.iloc[:, : user_df.columns.get_loc(column)],
         pd.get_dummies(user_df[column], prefix=str(column)),
-        user_df.iloc[:, user_df.columns.get_loc(column) + 1 :]
+        user_df.iloc[:, user_df.columns.get_loc(column) + 1:]
     ], axis=1)
 
 
-#Import Tappy Data                    
+# Import Tappy Data                    
 column_names = [first_hand + second_hand + '_' + time for first_hand in ['L', 'R', 'S'] for second_hand in ['L', 'R', 'S'] for time in ['Hold time', 'Latency time', 'Flight time']]
 
 user_tappy_df = pd.DataFrame(columns=column_names)
@@ -74,180 +82,218 @@ for user_id in user_df.index:
     user_tappy_df.loc[user_id] = user_tappy_data
 
 user_tappy_df = user_tappy_df.fillna(0)
-user_tappy_df[user_tappy_df < 0] = 0  
-#%% Data Visualisation: User Data
+user_tappy_df[user_tappy_df < 0] = 0
+
+# %% Data Visualisation: User Data
 N = len(users_actual)
 
-#Missing Data
+# Missing Data
 missing_data = user_df.isnull().sum()
-g = sns.barplot([missing_data.index[0],missing_data.index[4]],[missing_data.BirthYear,missing_data.DiagnosisYear],palette='bwr_r')
-cBirthyear = round(missing_data.BirthYear/(N/100),2)
+g = sns.barplot([missing_data.index[0], missing_data.index[4]],
+                [missing_data.BirthYear, missing_data.DiagnosisYear],
+                palette='bwr_r')
+cBirthyear = round(missing_data.BirthYear/(N/100), 2)
 cBirthyear = str(cBirthyear) + '%'
-cDiagnosisyear = round(missing_data.DiagnosisYear/(N/100),2)
+cDiagnosisyear = round(missing_data.DiagnosisYear/(N/100), 2)
 cDiagnosisyear = str(cDiagnosisyear) + '%'
-g.text(0,40,cBirthyear,horizontalalignment='center',fontsize=20)
-g.text(1,72,cDiagnosisyear,horizontalalignment='center',fontsize=20)
-plt.ylim([0,85])
+g.text(0, 40, cBirthyear, horizontalalignment='center', fontsize=20)
+g.text(1, 72, cDiagnosisyear, horizontalalignment='center', fontsize=20)
+plt.ylim([0, 85])
 plt.title('Missing Data, N = '+str(len(users_actual)))
-plt.savefig('missingdata.png',bbox_inches='tight')
+plt.savefig('missingdata.png', bbox_inches='tight')
 plt.show()
 
-#People with Parkinsons
-g = sns.countplot(user_df.Parkinsons,palette='bwr_r')
-cParkinsonsFalse = round(user_df.Parkinsons[user_df['Parkinsons']==0].count()/(N/100),2)
+# People with Parkinsons
+g = sns.countplot(user_df.Parkinsons, palette='bwr_r')
+cParkinsonsFalse = round(user_df.Parkinsons[user_df['Parkinsons']==0].count()/(N/100), 2)
 cParkinsonsFalse = str(cParkinsonsFalse) + '%'
-cParkinsonsTrue = round(user_df.Parkinsons[user_df['Parkinsons']==1].count()/(N/100),2)
+cParkinsonsTrue = round(user_df.Parkinsons[user_df['Parkinsons']==1].count()/(N/100), 2)
 cParkinsonsTrue = str(cParkinsonsTrue) + '%'
-g.text(0,65,cParkinsonsFalse,horizontalalignment='center',fontsize=20)
-g.text(1,170,cParkinsonsTrue,horizontalalignment='center',fontsize=20)
-plt.ylim([0,190])
+g.text(0, 65, cParkinsonsFalse, horizontalalignment='center', fontsize=20)
+g.text(1, 170, cParkinsonsTrue, horizontalalignment='center', fontsize=20)
+plt.ylim([0, 190])
 plt.title('Number of people with Parkinsons')
-plt.savefig('countparkinsons.png',bbox_inches='tight')
+plt.savefig('countparkinsons.png', bbox_inches='tight')
 plt.show()
 
-#Males and Females
-g = sns.countplot(user_df.Male,palette='bwr_r')
-cMale = round(user_df.Male[user_df['Male']==1].count()/(N/100),2)
+# Males and Females
+g = sns.countplot(user_df.Male, palette='bwr_r')
+cMale = round(user_df.Male[user_df['Male']==1].count()/(N/100), 2)
 cMale = str(cMale) + '%'
-cFemale = round(user_df.Male[user_df['Male']==0].count()/(N/100),2)
+cFemale = round(user_df.Male[user_df['Male']==0].count()/(N/100), 2)
 cFemale = str(cFemale) + '%'
-g.text(0,105,cFemale,horizontalalignment='center',fontsize=20)
-g.text(1,120,cMale,horizontalalignment='center',fontsize=20)
-plt.ylim([0,140])
+g.text(0, 105, cFemale, horizontalalignment='center', fontsize=20)
+g.text(1, 120, cMale, horizontalalignment='center', fontsize=20)
+plt.ylim([0, 140])
 plt.title('Number of males and females')
-plt.savefig('countgender.png',bbox_inches='tight')
+plt.savefig('countgender.png', bbox_inches='tight')
 plt.show()
 
 #Males and Females for Parkinson: Stacked
 ind = np.arange(2)
 width = 0.5
 
-Ptrue_M = user_df[user_df['Parkinsons']==1]
-Ptrue_M = len(Ptrue_M.Male[Ptrue_M['Male']==1])
-Ptrue_F = user_df[user_df['Parkinsons']==1]
-Ptrue_F = len(Ptrue_F.Male[Ptrue_F['Male']==0])
+Ptrue_M = user_df[user_df['Parkinsons'] == 1]
+Ptrue_M = len(Ptrue_M.Male[Ptrue_M['Male'] == 1])
+Ptrue_F = user_df[user_df['Parkinsons'] == 1]
+Ptrue_F = len(Ptrue_F.Male[Ptrue_F['Male'] == 0])
 
-Pfalse_M = user_df[user_df['Parkinsons']==0]
-Pfalse_M = len(Pfalse_M.Male[Pfalse_M['Male']==1])
-Pfalse_F = user_df[user_df['Parkinsons']==0]
-Pfalse_F = len(Pfalse_F.Male[Pfalse_F['Male']==0])
+Pfalse_M = user_df[user_df['Parkinsons'] == 0]
+Pfalse_M = len(Pfalse_M.Male[Pfalse_M['Male'] == 1])
+Pfalse_F = user_df[user_df['Parkinsons'] == 0]
+Pfalse_F = len(Pfalse_F.Male[Pfalse_F['Male'] == 0])
 
-men = (Pfalse_M,Ptrue_M)
-women = (Pfalse_F,Ptrue_F)
+men = (Pfalse_M, Ptrue_M)
+women = (Pfalse_F, Ptrue_F)
 
-with sns.color_palette(palette='bwr',n_colors=2): #Men in blue, women in pink
-    g1 = plt.bar(ind,men,width)
-    g2 = plt.bar(ind,women,width,bottom=men)
-plt.xticks(ind, ('Healthy','Parkinsons'))
+with sns.color_palette(palette='bwr', n_colors=2):  # Men in blue, women in pink
+    g1 = plt.bar(ind, men, width)
+    g2 = plt.bar(ind, women, width, bottom=men)
+plt.xticks(ind, ('Healthy', 'Parkinsons'))
 plt.ylim([0, 200])
-plt.text(0, 65,cParkinsonsFalse,horizontalalignment='center',fontsize=20)
-plt.text(1, 170,cParkinsonsTrue,horizontalalignment='center',fontsize=20)
+plt.text(0, 65, cParkinsonsFalse, horizontalalignment='center', fontsize=20)
+plt.text(1, 170, cParkinsonsTrue, horizontalalignment='center', fontsize=20)
 plt.title('Distribution of gender among Parkinsons and Healthy')
 plt.show()
 
-#Males and Females for Parkinson
-g = sns.countplot(x='Parkinsons',hue='Male',data=user_df,palette='bwr_r')
-cPM_false = user_df[user_df['Parkinsons']==0]
-cPM_false = str(round(cPM_false.Male[cPM_false['Male']==1].count()/(cPM_false.Parkinsons.count()/100)))+'%'
-cPM_true = user_df[user_df['Parkinsons']==1]
-cPM_true = str(round(cPM_true.Male[cPM_true['Male']==1].count()/(cPM_true.Parkinsons.count()/100)))+'%'
-cPF_false = user_df[user_df['Parkinsons']==0]
-cPF_false = str(round(cPF_false.Male[cPF_false['Male']==0].count()/(cPF_false.Parkinsons.count()/100)))+'%' 
-cPF_true = user_df[user_df['Parkinsons']==1]
-cPF_true = str(round(cPF_true.Male[cPF_true['Male']==0].count()/(cPF_true.Parkinsons.count()/100)))+'%'
-g.text(-0.35,28,cPF_false,fontsize=15)
-g.text(0.05,35,cPM_false,fontsize=15)
-g.text(0.65,80,cPF_true,fontsize=15)
-g.text(1.05,88,cPM_true,fontsize=15)
+# Males and Females for Parkinson
+g = sns.countplot(x='Parkinsons', hue='Male', data=user_df, palette='bwr_r')
+cPM_false = user_df[user_df['Parkinsons'] == 0]
+cPM_false = str(round(cPM_false.Male[cPM_false['Male'] == 1].count()/(cPM_false.Parkinsons.count()/100)))+'%'
+cPM_true = user_df[user_df['Parkinsons'] == 1]
+cPM_true = str(round(cPM_true.Male[cPM_true['Male'] == 1].count()/(cPM_true.Parkinsons.count()/100)))+'%'
+cPF_false = user_df[user_df['Parkinsons'] == 0]
+cPF_false = str(round(cPF_false.Male[cPF_false['Male'] == 0].count()/(cPF_false.Parkinsons.count()/100)))+'%' 
+cPF_true = user_df[user_df['Parkinsons'] == 1]
+cPF_true = str(round(cPF_true.Male[cPF_true['Male'] == 0].count()/(cPF_true.Parkinsons.count()/100)))+'%'
+g.text(-0.35, 28, cPF_false, fontsize=15)
+g.text(0.05, 35, cPM_false, fontsize=15)
+g.text(0.65, 80, cPF_true, fontsize=15)
+g.text(1.05, 88, cPM_true, fontsize=15)
 plt.ylim([0, 110])
 plt.title('Distribution of gender for Parkinsons')
-plt.savefig('parkinsonscountgender.png',bbox_inches='tight')
+plt.savefig('parkinsonscountgender.png', bbox_inches='tight')
 plt.show()
 
-#Tremors for Parkinsons
-g = sns.countplot(x='Parkinsons',hue='Tremors',data=user_df,palette='bwr_r')
+# Tremors for Parkinsons
+g = sns.countplot(x='Parkinsons', hue='Tremors', data=user_df, palette='bwr_r')
 plt.title('Distribution of tremors for Parkinsons')
-plt.savefig('parkinsonscounttremors.png',bbox_inches='tight')
+plt.savefig('parkinsonscounttremors.png', bbox_inches='tight')
 plt.show()
 
-#Impact
-g = sns.countplot(x='Impact',hue='Parkinsons',order=['None','Mild','Medium','Severe'],data=user_df,palette='bwr_r')
-cNone = str(round(user_df.Impact[user_df['Impact']=='None'].count()/(N/100),2)) + '%'
-cMild = str(round(user_df.Impact[user_df['Impact']=='Mild'].count()/(N/100),2)) + '%'
-cMedium = str(round(user_df.Impact[user_df['Impact']=='Medium'].count()/(N/100),2)) + '%'
-cSevere = str(round(user_df.Impact[user_df['Impact']=='Severe'].count()/(N/100),2)) + '%'
+# Impact
+g = sns.countplot(x='Impact', hue='Parkinsons',
+                  order=['None', 'Mild', 'Medium', 'Severe'],
+                  data=user_df, palette='bwr_r')
+cNone = str(round(user_df.Impact[user_df['Impact'] == 'None'].count()/(N/100), 2)) + '%'
+cMild = str(round(user_df.Impact[user_df['Impact'] == 'Mild'].count()/(N/100), 2)) + '%'
+cMedium = str(round(user_df.Impact[user_df['Impact'] == 'Medium'].count()/(N/100), 2)) + '%'
+cSevere = str(round(user_df.Impact[user_df['Impact'] == 'Severe'].count()/(N/100), 2)) + '%'
 plt.title('Severity of Parkinsons')
-g.text(-0.4,55,cNone,fontsize=12)
-g.text(1,67,cMild,fontsize=12)
-g.text(2,72,cMedium,fontsize=12)
-g.text(3,27,cSevere,fontsize=12)
-plt.ylim([0,90])
-plt.savefig('countimpact.png',bbox_inches='tight')
+g.text(-0.4, 55, cNone, fontsize=12)
+g.text(1, 67, cMild, fontsize=12)
+g.text(2, 72, cMedium, fontsize=12)
+g.text(3, 27, cSevere, fontsize=12)
+plt.ylim([0, 90])
+plt.savefig('countimpact.png', bbox_inches='tight')
 plt.show()
 
-#%% Data Visualisation: Tappy Data
+# %% Data Visualisation: Tappy Data
 combined_user_df = pd.concat([user_df, user_tappy_df], axis=1)
 
-total_col_names = ['Hold time','Latency time','Flight time']
-total_hand_names = ['LL','LR','LS','RL','RR','RS','SL','SR','SS']
+total_col_names = ['Hold time', 'Latency time', 'Flight time']
+total_hand_names = ['LL', 'LR', 'LS', 'RL', 'RR', 'RS', 'SL', 'SR', 'SS']
 N = len(total_hand_names)
 for col in total_col_names:
     for col2 in total_hand_names:
-        combined_user_df[col] = ((combined_user_df['LL_'+col]+
-                                 combined_user_df['LR_'+col]+
-                                 combined_user_df['LS_'+col]+
-                                 combined_user_df['RL_'+col]+
-                                 combined_user_df['RR_'+col]+
-                                 combined_user_df['RS_'+col]+
-                                 combined_user_df['SL_'+col]+
-                                 combined_user_df['SR_'+col]+
+        combined_user_df[col] = ((combined_user_df['LL_'+col] +
+                                 combined_user_df['LR_'+col] +
+                                 combined_user_df['LS_'+col] +
+                                 combined_user_df['RL_'+col] +
+                                 combined_user_df['RR_'+col] +
+                                 combined_user_df['RS_'+col] +
+                                 combined_user_df['SL_'+col] +
+                                 combined_user_df['SR_'+col] +
                                  combined_user_df['SS_'+col])/N)
 combined_user_df[combined_user_df['Hold time'] > 800] = 0
 combined_user_df[combined_user_df['Latency time'] > 800] = 0
 combined_user_df[combined_user_df['Flight time'] > 800] = 0
 
-all_cols = ['BirthYear','Male','Parkinsons','Tremors','DiagnosisYear','Sided_Left','Sided_None','Sided_Right','UPDRS_1','UPDRS_2','UPDRS_3','UPDRS_4',"UPDRS_Don't know",'Impact','Levadopa','DA','MAOB','Other']+column_names
-combined_user_df_copy = combined_user_df.melt(id_vars=all_cols,value_vars=['Hold time','Latency time','Flight time'],var_name='Kind',value_name='Time')
+all_cols = (['BirthYear', 'Male', 'Parkinsons', 'Tremors', 'DiagnosisYear',
+             'Sided_Left', 'Sided_None', 'Sided_Right', 'UPDRS_1', 'UPDRS_2',
+             'UPDRS_3', 'UPDRS_4', "UPDRS_Don't know", 'Impact',
+             'Levadopa', 'DA', 'MAOB', 'Other'] + column_names)
+combined_user_df_copy = combined_user_df.melt(id_vars=all_cols,
+                                              value_vars=['Hold time',
+                                                          'Latency time',
+                                                          'Flight time'],
+                                              var_name='Kind',
+                                              value_name='Time')
 
-combined_user_df[combined_user_df['Impact'] == 0] = None
-combined_user_df_copy[combined_user_df_copy['Impact'] == 0] = None
-order = ['None','Mild','Medium','Severe']
+combined_user_df[combined_user_df['Impact'] == 0] = 'None'
+combined_user_df_copy[combined_user_df_copy['Impact'] == 0] = 'None'
+order = ['None', 'Mild', 'Medium', 'Severe']
 
-#Boxplot, grouped by Parkinsons
-sns.boxplot(x='Kind',y='Time',hue='Parkinsons',data=combined_user_df_copy)
+# Boxplot, grouped by Parkinsons
+sns.boxplot(x='Kind', y='Time', hue='Parkinsons', data=combined_user_df_copy)
 plt.title('Mean times for Parkinsons')
 plt.ylabel('Time (ms)')
 plt.legend(loc='upper left')
-plt.savefig('swarmtimes.png',bbox_inces='tight')
+plt.savefig('swarmtimes.png', bbox_inces='tight')
 plt.show()
 
-#Boxplot, grouped by Impact
-sns.boxplot(x='Kind',y='Time',hue='Impact',hue_order=order,data=combined_user_df_copy)
+# Boxplot, grouped by Impact
+sns.boxplot(x='Kind', y='Time', hue='Impact',
+            hue_order=order, data=combined_user_df_copy)
 plt.title('Mean times for Parkinsons')
 plt.ylabel('Time (ms)')
-plt.savefig('impacttimes.png',bbox_inces='tight')
+plt.savefig('impacttimes.png', bbox_inces='tight')
 plt.show()
 
-#Scatterplot: Hold vs Latency
-sns.scatterplot(x='Hold time',y='Latency time',hue='Impact',hue_order=order,data=combined_user_df)
+# Scatterplot: Hold vs Latency
+sns.scatterplot(x='Hold time', y='Latency time',
+                hue='Impact', hue_order=order, data=combined_user_df)
 plt.ylabel('Latency time (ms)')
 plt.xlabel('Hold time (ms)')
 plt.title('Mean times of every participant')
-plt.savefig('scatterholdlatency.png',bbox_inces='tight')
+plt.savefig('scatterholdlatency.png', bbox_inces='tight')
 plt.show()
 
-#Scatterplot: Hold vs Flight
-sns.scatterplot(x='Hold time',y='Flight time',hue='Impact',hue_order=order,data=combined_user_df)
+# Scatterplot: Hold vs Flight
+sns.scatterplot(x='Hold time', y='Flight time',
+                hue='Impact', hue_order=order, data=combined_user_df)
 plt.ylabel('Flight (ms)')
 plt.xlabel('Hold time (ms)')
 plt.title('Mean times of every participant')
-plt.savefig('scatterholdflight.png',bbox_inces='tight')
+plt.savefig('scatterholdflight.png', bbox_inces='tight')
 plt.show()
 
-#Scatterplot: Flight vs Latency
-sns.scatterplot(x='Flight time',y='Latency time',hue='Impact',hue_order=order,data=combined_user_df)
+# Scatterplot: Flight vs Latency
+sns.scatterplot(x='Flight time', y='Latency time', hue='Impact',
+                hue_order=order, data=combined_user_df)
 plt.ylabel('Latency time (ms)')
 plt.xlabel('Flight time (ms)')
 plt.title('Mean times of every participant')
-plt.savefig('scatterlatencyflight.png',bbox_inces='tight')
+plt.savefig('scatterlatencyflight.png', bbox_inces='tight')
 plt.show()
+
+
+# %% Machine Learning
+target = 'Parkinsons'
+features = ['Male', 'Tremors', 'Sided_Left', 'Sided_Right', 'Sided_None',
+            'UPDRS_1', 'UPDRS_2', 'UPDRS_3', 'UPDRS_4', "UPDRS_Don't know",
+            'Impact', 'Hold time', 'Latency time', 'Flight time'] + column_names
+            
+X = combined_user_df[features]
+y = combined_user_df[target]
+
+## One Hot Encoder
+#features_OHE = ['Male', 'Tremors', 'Sided_Left', 'Sided_Right', 'Sided_None',
+#                'UPDRS_1', 'UPDRS_2', 'UPDRS_3', 'UPDRS_4']
+#OHE = OneHotEncoder()
+#X[features_OHE] = OHE.fit(X[features_OHE])
+#
+# Label Encoder
+features_LE = ['Impact']
+LE = LabelEncoder()
+X[features_LE] = LE.fit(X[features_LE]).transform(X[features_LE])
---
+++ ProjectParkinson_nieuw.py
@@ -0,0 +1,187 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 28 11:11:34 2019
+
+@author: s2589656
+"""
+import os
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from pandas.api.types import CategoricalDtype
+
+from readprocess_tappy import process_user
+
+import warnings; warnings.filterwarnings('ignore')
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import StandardScaler, RobustScaler, LabelEncoder
+from sklearn.model_selection import cross_val_score, train_test_split
+from sklearn.metrics import accuracy_score, roc_auc_score, roc_curve, confusion_matrix
+
+
+# %% Methods
+def model_pipeline(model, X, y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3,
+                                                        random_state=42)
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    return y_pred
+
+
+def model_evaluation(model, X, y, y_pred):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3,
+                                                        random_state=42)
+    cvs = cross_val_score(model, X, y, scoring='accuracy', cv=5)
+    acc = accuracy_score(y_test, y_pred)
+    probs = model.predict_proba(X_test)
+    fpr, tpr, threshold_knn = roc_curve(y_test, probs[:, 1])
+    auc = roc_auc_score(y_test, probs[:, 1])
+    conf = confusion_matrix(y_test, y_pred)
+    return cvs, acc, auc, fpr, tpr, conf
+
+# %% Directories
+directory_tappy = "Tappy Data/"
+directory_user = "User Data/"
+
+# %% Data Import
+user_data = os.listdir(directory_user)
+tappy_data = os.listdir(directory_tappy)
+
+ID_data = [x[5:15] for x in user_data] #Get user ID from file name
+ID_tappy = [x[0:10] for x in tappy_data]   
+
+users_actual = [value for value in ID_data if value in ID_tappy] #If the user has no Tappy data, we dont need that user
+
+# Import User Data
+medicines = ['Levadopa', 'DA', 'MAOB', 'Other']
+columns = ['BirthYear', 'Gender', 'Parkinsons', 'Tremors', 'DiagnosisYear',
+    'Sided', 'UPDRS', 'Impact', 'Levadopa', 'DA', 'MAOB', 'Other']
+user_df = pd.DataFrame(columns=columns)
+for userid in users_actual:
+    f = open(directory_user+'User_'+userid+'.txt')
+    user_df.loc[userid] = [line.split(': ')[1][: -1] for line in f.readlines()]
+    f.close()
+
+# Clean User Data
+user_df['BirthYear'] = pd.to_numeric(user_df['BirthYear'], errors='coerce')
+user_df['DiagnosisYear'] = pd.to_numeric(user_df['DiagnosisYear'], errors='coerce')
+user_df = user_df.rename(index=str, columns={'Gender': 'Male'}) # renaming `Gender` to `Female`
+user_df['Male'] = user_df['Male'] == 'Male' # change string data to boolean data
+user_df['Male'] = user_df['Male'].astype(int) # change boolean data to binary data
+str_to_bin_columns = ['Parkinsons', 'Tremors', 'Levadopa', 'DA', 'MAOB', 'Other'] # columns to be converted to binary data
+
+for column in str_to_bin_columns:
+    user_df[column] = user_df[column] == 'True'
+    user_df[column] = user_df[column].astype(int)
+
+user_df.loc[
+    (user_df['Impact'] != 'Medium') &
+    (user_df['Impact'] != 'Mild') &
+    (user_df['Impact'] != 'Severe'), 'Impact'] = 'No'
+
+to_dummy_column_indices = ['Sided', 'UPDRS','Impact'] # columns to be one-hot encoded
+for column in to_dummy_column_indices:
+    user_df = pd.concat([
+        user_df.iloc[:, : user_df.columns.get_loc(column)],
+        pd.get_dummies(user_df[column], prefix=str(column)),
+        user_df.iloc[:, user_df.columns.get_loc(column) + 1 :]
+    ], axis=1)
+
+# NA data in DiagnosisYear and BirthYear
+user_df['DiagnosisYear'] = user_df['DiagnosisYear'].fillna(0)
+user_df['BirthYear'] = user_df['BirthYear'].fillna(0)
+
+# Making everything an integer
+for column in user_df:
+    user_df[column] = user_df[column].astype(int)
+
+# %% Import Tappy Data
+tappy_names = ['L_Hand_mean', 'L_Hand_std', 'L_Hand_kurt', 'L_Hand_skew',
+               'R_Hand_mean', 'R_Hand_std', 'R_Hand_kurt', 'R_Hand_skew', 
+               'diff_Hand_mean',
+               'LR_mean', 'LR_std', 'LR_kurt', 'LR_skew',
+               'RL_mean', 'RL_std', 'RL_kurt', 'RL_skew',
+               'LL_mean', 'LL_std', 'LL_kurt', 'LL_skew',
+               'RR_mean', 'RR_std', 'RR_kurt', 'RR_skew',
+               'diff_opposite_mean', 'diff_same_mean']
+
+user_tappy_df = pd.DataFrame(columns=tappy_names)
+
+for user_id in user_df.index:
+    user_tappy_data = process_user(directory_tappy, str(user_id), tappy_data)
+    user_tappy_df.loc[user_id] = user_tappy_data
+
+# %%
+user_tappy_df.dropna(inplace=True)
+full_set = pd.merge(user_tappy_df.reset_index(), user_df.reset_index(), on='index')
+full_set.set_index('index', inplace=True)
+
+# %% Machine Learning
+features = ['L_Hand_mean', 'L_Hand_std', 'L_Hand_kurt', 'L_Hand_skew',
+               'R_Hand_mean', 'R_Hand_std', 'R_Hand_kurt', 'R_Hand_skew', 
+               'diff_Hand_mean',
+               'LR_mean', 'LR_std', 'LR_kurt', 'LR_skew',
+               'RL_mean', 'RL_std', 'RL_kurt', 'RL_skew',
+               'LL_mean', 'LL_std', 'LL_kurt', 'LL_skew',
+               'RR_mean', 'RR_std', 'RR_kurt', 'RR_skew',
+               'diff_opposite_mean', 'diff_same_mean',
+               'Impact_Medium','Impact_Mild','Impact_No','Impact_Severe',
+               'Levadopa',
+               'Sided_Left', 'Sided_Right', 'Sided_None']
+target = 'Parkinsons'
+
+X = full_set[features]
+X = RobustScaler().fit_transform(X)
+y = full_set[target]
+
+knn = KNeighborsClassifier(n_neighbors=2)
+logreg = LogisticRegression()
+tree = DecisionTreeClassifier(max_depth=2)
+
+y_pred_knn = model_pipeline(knn, X, y)
+y_pred_logreg = model_pipeline(logreg, X, y)
+y_pred_tree = model_pipeline(tree, X, y)
+
+# %% Evaluation
+cvs_knn, acc_knn, auc_knn, fpr_knn, tpr_knn, conf_knn = model_evaluation(knn, X, y, y_pred_knn)
+cvs_logreg, acc_logreg, auc_logreg, fpr_logreg, tpr_logreg, conf_logreg = model_evaluation(logreg, X, y, y_pred_logreg)
+cvs_tree, acc_tree, auc_tree, fpr_tree, tpr_tree, conf_tree = model_evaluation(tree, X, y, y_pred_tree)
+
+# %% Plotting
+# Accuracy scores, tpr, fpr from Schrag et al (2002)
+acc_all = (111+54)/200
+acc_PD = (86+20)/123
+acc_nonPD = (25+34)/77
+fpr_all = 20/(20+54)
+fpr_PD = 11/(11+20)
+fpr_nonPD = 9/(9+34)
+tpr_all = 111/(111+15)
+tpr_PD = 86/(86+6)
+tpr_nonPD = 25/(25+9)
+xmargin = 0.02
+ymargin = -0.02
+
+plt.plot(fpr_knn, tpr_knn, label='knn')
+plt.plot(fpr_logreg, tpr_logreg, label='logreg')
+plt.plot(fpr_tree, tpr_tree, label='tree')
+plt.plot([0, 1], [0, 1], color='black', linestyle='--', label='no skill ref')
+plt.plot([fpr_all, fpr_PD, fpr_nonPD], [tpr_all, tpr_PD, tpr_nonPD], 'ro')
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Classifier Comparison')
+plt.text(0.74, 0.5, 'AUC knn: {:.3f}'.format(auc_knn))
+plt.text(0.74, 0.4, 'AUC logreg: {:.3f}'.format(auc_logreg))
+plt.text(0.74, 0.6, 'AUC tree: {:.3f}'.format(auc_tree))
+plt.text(fpr_all + xmargin, tpr_all + ymargin, 'All')
+plt.text(fpr_PD + xmargin, tpr_PD + ymargin, 'Specialists')
+plt.text(fpr_nonPD + xmargin, tpr_nonPD + ymargin, 'non-Specialists')
+plt.legend()
+plt.savefig('AUCclassifiers.png',bbox_inches='tight')
+plt.show()
